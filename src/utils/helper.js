eb8fa486eaa7066179f0c8ceb4f7de0bb8f021e2
prashant-Kumar <prashant0kumar101@gmail.com>
2025-03-10 22:21:55 +0530

    Refactor token generation to use centralized config for secret and expiration

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
-import JWT_SECRET from '../config/dotenv.js'
-import JWT_EXPIRES_IN from '../config/dotenv.js'
+import config from "../config/dotenv.js";
 
 export const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN || '7d' });
+  return jwt.sign({ id: user._id }, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRES_IN || '7d' });
 };
